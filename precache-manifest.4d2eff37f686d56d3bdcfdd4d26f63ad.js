8c008ef480cc1200b7ac54e833d1f83247fa3b45
Alexander Zhmurkov <a.zhmurkov@mail.ru>
2024-02-04 17:55:41 +0300

    Updates

---
+++ precache-manifest.4d2eff37f686d56d3bdcfdd4d26f63ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c360417770a40ce19caae50c54b973a",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "2b1b67fb0ac9e8eb045f",
+    "url": "/react-shop/static/css/main.3e7e9d52.chunk.css"
+  },
+  {
+    "revision": "fff5291e32c79c1aef08",
+    "url": "/react-shop/static/js/2.94593e27.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-shop/static/js/2.94593e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1b67fb0ac9e8eb045f",
+    "url": "/react-shop/static/js/main.23d055f9.chunk.js"
+  },
+  {
+    "revision": "97dbaf63fab789531e02",
+    "url": "/react-shop/static/js/runtime-main.f2e75520.js"
+  }
+]);
